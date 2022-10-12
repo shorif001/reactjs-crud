ee5d562e191957943a2e7241d69abddfe8d128f9
shorif <sashipon001@gmail.com>
2022-10-12 10:11:16 +0600

    login page added

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -13,14 +13,15 @@ const Login = () => {
           <div className="col-md-6">
             <form>
               <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <label class="form-label">Email address</label>
                 <input type="email" class="form-control" />
               </div>
               <div class="mb-3">
                 <label class="form-label">Password</label>
                 <input type="password" class="form-control" />
+                <i class="bi bi-eye-fill"></i>
+                <i class="bi bi-eye-slash-fill"></i>
               </div>
-
               <button type="submit" class="btn btn-primary">Submit</button>
             </form>
           </div>
